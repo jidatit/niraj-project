80cc6e33dff69f48a1d646f32983c4b6e2b32bb4
Raja Shahpaal <91369872+Etzshally@users.noreply.github.com>
2024-04-17 13:06:10 +0500

    logo in footer

--- src/web/components/Footer.jsx
+++ src/web/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import logo from "../../assets/newlogo.png"
 
 const Footer = () => {
   return (
     <>
       <footer className="bg-[#003049] flex flex-col justify-center items-center h-[436px] py-6">
         <div className="container text-white mx-auto flex flex-col gap-6 justify-center items-center">
-          <div className="mb-4 md:mb-0">
-            <p className="text-center md:text-[30px] font-bold text-[20px]">John Galt Thaker Insurance Branch</p>
+          <div className="mb-4 gap-5 flex flex-col justify-center items-center md:mb-0">
+            <img className='w-[60%] md:w-[50%]' src={logo} alt="" />
             <ul className="flex justify-center">
               <li className="mr-4"><a className='md:text-[20px] underline font-semibold text-[17px]' href="#">Home</a></li>
               <li className="mr-4"><a className='md:text-[20px] underline font-semibold text-[17px]' href="#">Free Quote</a></li>
