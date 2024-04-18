136e39d9dad5390d83d5189d0017dae507434957
Raja Shahpaal <91369872+Etzshally@users.noreply.github.com>
2024-04-18 10:04:02 +0500

    back button

--- src/auth/Layout.jsx
+++ src/auth/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet, Navigate, Link } from 'react-router-dom'
 import { useAuth } from "../AuthContext";
 
 const Loader = () => {
@@ -18,7 +18,14 @@ const Layout = () => {
     return (
         !currentUser ? (
             <>
-                <div className='w-full min-h-screen flex flex-col justify-center items-center bg-[#003049]'>
+                <div className='w-full relative min-h-screen flex flex-col justify-center items-center bg-[#003049]'>
+                    <Link to="/">
+                        <button className='absolute text-white rounded-full bg-white/20 shadow-lg isolate backdrop-blur-3xl px-3 py-3 transition-all ease-in-out delay-100 hover:pr-[50px] hover:ring-2 left-[50px] top-[20px]'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+                            </svg>
+                        </button>
+                    </Link>
                     <Outlet />
                 </div>
             </>) :
