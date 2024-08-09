de243db0c12d922345155aff6b53a57ecd3afe26
Raja Shahpaal <91369872+Etzshally@users.noreply.github.com>
2024-08-09 11:51:43 +0500

    bug: fixes in the submit buttons to disable them while they are being submitted

--- Frontend/src/admin_portal/components/Button.jsx
+++ Frontend/src/admin_portal/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({text, onClickProp}) => {
+const Button = ({ isDisabled = false, text, onClickProp }) => {
   return (
-    <button onClick={onClickProp} className='w-full h-[50px] rounded-lg bg-gradient-to-r from-[#17A600] to-[#17A600] font-bold text-[20px] text-white'>{text}</button>
+    <button disabled={isDisabled} onClick={onClickProp} className='w-full h-[50px] rounded-lg bg-gradient-to-r from-[#17A600] to-[#17A600] font-bold text-[20px] text-white'>{text}</button>
   )
 }
 
