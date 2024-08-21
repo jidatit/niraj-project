3dba75fb651d1e052ac99d087e6ebd17b9741143
Zohaib Haider <zebihaider123@gmail.com>
2024-08-21 20:37:43 +0500

    changes

--- Frontend/src/auth/Layout.jsx
+++ Frontend/src/auth/Layout.jsx
@@ -1,38 +1,47 @@
-import React from 'react';
-import { Outlet, Navigate, Link } from 'react-router-dom'
+import React from "react";
+import { Outlet, Navigate, Link } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 
 const Loader = () => {
-    return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-200 opacity-75 z-50">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-        </div>
-    );
-}
+  return (
+    <div className="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-gray-200 opacity-75">
+      <div className="w-32 h-32 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  );
+};
 
 const Layout = () => {
-    const { currentUser, loading } = useAuth();
-    if (loading) {
-        return <Loader />;
-    }
-    return (
-        !currentUser ? (
-            <>
-                <div className='w-full relative min-h-screen flex flex-col justify-center items-center bg-[#003049]'>
-                    <Link to="/">
-                        <button className='absolute text-white rounded-full bg-white/20 shadow-lg isolate backdrop-blur-3xl px-3 py-3 transition-all ease-in-out delay-100 hover:pr-[50px] hover:ring-2 left-[50px] top-[20px]'>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
-                            </svg>
-                        </button>
-                    </Link>
-                    <Outlet />
-                </div>
-            </>) :
-            (
-                <Navigate to="/" />
-            )
-    )
-}
+  const { currentUser, loading } = useAuth();
+  if (loading) {
+    return <Loader />;
+  }
+  return !currentUser ? (
+    <>
+      <div className="w-full relative min-h-screen flex flex-col justify-center items-center bg-[#003049]">
+        <Link to="/">
+          <button className="absolute text-white rounded-full bg-white/20 shadow-lg isolate backdrop-blur-3xl px-3 py-3 transition-all ease-in-out delay-100 hover:pr-[50px] hover:ring-2 left-[50px] top-[20px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+              />
+            </svg>
+          </button>
+        </Link>
+        <Outlet />
+      </div>
+    </>
+  ) : (
+    <Navigate to="/" />
+  );
+};
 
-export default Layout
+export default Layout;
